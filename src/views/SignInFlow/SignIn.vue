913297958e0f18651e97e09286ee598dc15701a3
Wang-Zhenjie <wangzhenjie@outlook.com>
2020-04-18 18:26:32 +0800

    2020/04/18-1st

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -26,67 +26,30 @@
         :class="{ 'light-recover': !isDarkMode, 'dark-recover': isDarkMode }"
         >忘记密码？</router-link
       >
-      <button @click="toggleDarkMode">切换显示模式</button>
+      <ThemeSwitch />
     </div>
   </div>
 </template>
 
 <script>
 import RequestAccount from "@/components/RequestAccount.vue";
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
 
 export default {
   name: "SignIn",
   components: {
-    RequestAccount
+    RequestAccount,
+    ThemeSwitch
   },
   computed: {
     isDarkMode() {
       return this.$store.getters.isDarkMode;
-    }
-  },
-  methods: {
-    toggleDarkMode() {
-      this.$store.commit("toggleDarkMode");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.light-background {
-  background-color: $light-gray;
-}
-
-.dark-background {
-  background-color: $dark-blue;
-}
-
-.light-text {
-  color: $black;
-}
-
-.dark-text {
-  color: $white;
-}
-
-.light-field {
-  background-color: rgba(198, 208, 235, 0.2);
-  border: 1px solid rgba(19, 16, 16, 0.2);
-
-  &::placeholder {
-    color: rgba(0, 0, 0, 0.3);
-  }
-}
-
-.dark-field {
-  background-color: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
 .container {
   display: flex;
   justify-content: center;
@@ -102,79 +65,5 @@ export default {
     width: 120px;
     height: 120px;
   }
-}
-
-h4 {
-  margin: 0;
-  font-size: 24px;
-  line-height: 34px;
-  text-align: center;
-
-  color: #ffffff;
-}
-
-input {
-  margin-top: 20px;
-  padding: 0 20px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 6px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  height: 60px;
-  width: 100%;
-
-  font-size: 20px;
-  color: white;
-}
-
-button {
-  background-color: #56ccf2;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-  border-radius: 6px;
-  border: none;
-  margin-top: 20px;
-  margin-bottom: 20px;
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover {
-    background-color: #41a7c9;
-  }
-
-  &:active {
-    background-color: #207c9b;
-  }
-
-  height: 60px;
-  width: 100%;
-
-  font-size: 20px;
-  color: #ffffff;
-}
-
-a {
-  line-height: 25px;
-  font-size: 16px;
-  text-align: center;
-  text-decoration: none;
-}
-
-.light-recover {
-  color: rgba(0, 0, 0, 0.3);
-}
-
-.dark-recover {
-  color: rgba(255, 255, 255, 0.3);
 }
 </style>
---
+++ src/views/Team.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <h2>Team</h2>
+    <div class="cards">
+      <transition appear appear-active-class="animated flipInX">
+        <a
+          class="card card-1"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        ></a>
+      </transition>
+      <transition appear appear-active-class="animated flipInX">
+        <a
+          class="card card-2"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        ></a>
+      </transition>
+      <transition appear appear-active-class="animated flipInX">
+        <a
+          class="card card-3"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        ></a>
+      </transition>
+      <transition appear appear-active-class="animated flipInX">
+        <a
+          class="card card-4"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        ></a>
+      </transition>
+      <transition appear appear-active-class="animated flipInX">
+        <a
+          class="card card-5"
+          :class="{ 'light-card': !isDarkMode, 'dark-card': isDarkMode }"
+        ></a>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Team",
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  }
+};
+</script>
+
+<style scope lang="scss">
+.container {
+  margin: 0 25px;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: space-evenly;
+}
+
+.card {
+  height: 50px;
+  width: 50px;
+  background-color: red;
+  margin: 20px;
+}
+
+.card-1 {
+  animation-delay: 0;
+}
+
+.card-2 {
+  animation-delay: 0.5s;
+}
+
+.card-3 {
+  animation-delay: 1s;
+}
+
+.card-4 {
+  animation-delay: 1.5s;
+}
+
+.card-5 {
+  animation-delay: 2s;
+}
+
+.light-card {
+  background-color: $white;
+}
+
+.dark-card {
+  background-color: $super-dark-blue;
+}
+</style>
